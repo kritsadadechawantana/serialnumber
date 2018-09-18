fc42ca3d7abcfc8c744649e6be966c877186a335
kritsada <jokerstudio@hotmail.com>
2018-09-19 00:39:37 +0700

    cleaning code

--- src/Calculator/SerialCalculator.cs
+++ src/Calculator/SerialCalculator.cs
@@ -14,16 +14,18 @@ namespace Calculator
                 var serialNumber = new List<long>();
                 var baseNumber = number / range;
                 long start = (range % 2) == 0 ? baseNumber - ((range / 2) - 1) : baseNumber - (range / 2);
+                
                 if(start <= 0)
                 {
                     serialNumber.Add(number);
                     return serialNumber.ToArray();
+                }  
+                
+                for (long currentNumber = start; currentNumber < (start + range); currentNumber++)
+                {
+                    serialNumber.Add(currentNumber);
                 }
                 
-                for (long i = start; i < (start + range); i++)
-                {
-                    serialNumber.Add(i);
-                }
                 var result = serialNumber.Sum(it => it);
                 if (result == number) return serialNumber.ToArray();
                 range++;
